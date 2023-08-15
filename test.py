e7169e2cd6260927d15f93c0e10804db9beaef57
VINAY H R <vinayhr1211@gmail.com>
2023-08-15 14:19:47 +0530

    Added to test files to experiment more using them

--- test.py
+++ test.py
@@ -16,16 +16,16 @@ def rmse(predictions, y_test):
 def LSTM_mod(x_train, y_train, x_test):
     model = Sequential()
     model.add(LSTM(128, return_sequences=True, input_shape= (x_train.shape[1], 1)))
-    model.add(LSTM(128, return_sequences=True))
     model.add(LSTM(128, return_sequences=False))
-    model.add(Dense(80))
+    #model.add(Dense(80))
+    model.add(Dense(60))
     model.add(Dense(40))
     model.add(Dense(20))
     model.add(Dense(1))
     # Compile the model
     model.compile(optimizer='adam', loss='mean_squared_error')
     # Train the model
-    model.fit(x_train, y_train, batch_size=1, epochs=5)
+    model.fit(x_train, y_train, batch_size=32, epochs=30)
     predictions_scaled = model.predict(x_test)
     predictions = scaler.inverse_transform(predictions_scaled)
     return predictions, model
@@ -88,7 +88,7 @@ def save_model(model):
 model1 = load_model('model_rmse_3.010_amazon.h5')
 
 # Read data from CSV file
-df = pd.read_csv('.\\CSV Files\\TATAMOTORS.NS.csv')
+df = pd.read_csv('.\\CSV Files\\NVD.DE.csv')
 
 df['Date'] = pd.to_datetime(df['Date'])
 df.set_index('Date', inplace=True)
@@ -108,37 +108,37 @@ train_data = scaled_data[0:int(training_data_len), :]
 # Get the models predicted price values 
 x_train, y_train, x_test, y_test = training(training_data_len, train_data)
 
-LSTM_predictions = LSTM_model(x_test)
+#LSTM_predictions = LSTM_model(x_test)
 
 LSTM_preds, model = LSTM_mod(x_train, y_train, x_test)
 
-plot_model(LSTM_predictions, training_data_len)
+#plot_model(LSTM_predictions, training_data_len)
 
 plot_model(LSTM_preds, training_data_len)
 
-LSTM_predictions_list = LSTM_predictions.tolist()
+#LSTM_predictions_list = LSTM_predictions.tolist()
 LSTM_preds_list = LSTM_preds.tolist()
 y_test = y_test.tolist()
 
-l = list(zip(LSTM_predictions_list, y_test))
+#l = list(zip(LSTM_predictions_list, y_test))
 
 m = list(zip(LSTM_preds, y_test))
 
-print("\n\n\nPrice predictions and actual comparison from model loaded\n\n")
+'''print("\n\n\nPrice predictions and actual comparison from model loaded\n\n")
 
 for i in range(len(l)):
     print(l[i][0][0], l[i][1][0], sep = ' ')
-
+'''
 print("\n\n\nPrice predictions and actual comparison from the model built\n\n")
 
 for i in range(len(m)):
     print(m[i][0][0], m[i][1][0], sep = ' ')
 
 build_mod_rmse = rmse(LSTM_preds, y_test)
-save_model_rmse = rmse(LSTM_predictions, y_test)
+#save_model_rmse = rmse(LSTM_predictions, y_test)
 
-if build_mod_rmse < save_model_rmse:
-    save_model(model)
+'''if build_mod_rmse < save_model_rmse:
+    save_model(model)'''
 
-print("\n\nPrint Root mean squared error from model loaded", save_model_rmse)
+#print("\n\nPrint Root mean squared error from model loaded", save_model_rmse)
 print("\n\nPrint Root mean squared error from model built", build_mod_rmse)
